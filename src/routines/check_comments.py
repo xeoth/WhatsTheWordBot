eb8a46a2b0546d7cfe01519ac9d05933102d9d49
Xeoth <46075322+Xeoth@users.noreply.github.com>
2021-01-06 17:42:07 +0100

    moved first section of `run()` to a separate file

--- src/routines/check_comments.py
+++ src/routines/check_comments.py
@@ -14,12 +14,66 @@
 #
 #  ---
 #
-#  Last modified by Xeoth on 2.1.2021
+#  Last modified by Xeoth on 06.01.2021
 #                   ^--------^ please change when modifying to comply with the license
 
-import praw
-from os import getenv
+import logging
+from praw import exceptions
+import helpers
 
 
-def check_comments(reddit: praw.Reddit, db, config):
-    pass
+def check_comments(db: helpers.DatabaseHelper, rh: helpers.RedditHelper, config):
+    subreddit = config["subreddit"]
+
+    # check if any new comments, update submissions accordingly
+    comment_stream = subreddit.comments(limit=50)
+    for comment in comment_stream:
+        # checking whether we have all necessary values, as the post could've been deleted + some prerequisites
+        try:
+            assert comment and comment.author and comment.submission and comment.submission.author
+        except AssertionError:
+            break
+        # much better than a big if!
+        
+        # we don't want to get in the way of mods
+        if comment.author.name == 'AutoModerator':
+            break
+        elif rh.mod_overridden(comment.submission):
+            break
+
+        # on new comments made by OP
+        if comment.author.name == comment.submission.author.name:
+            # if OP's comment is "solved", flair submission as "solved"
+            if not rh.already_solved(comment.submission) and rh.solved_in_comment(comment):
+                try:
+                    db.save_post(comment.submission.id, 'solved')
+                    rh.apply_flair(
+                        submission=comment.submission, text=config["flairs"]["solved"]["text"],
+                        flair_id=config["flairs"]["solved"]["id"])
+                except exceptions.PRAWException:
+                    logging.error(
+                        f"Couldn't flair submission {comment.submission.id} as 'solved' following OP's new comment.")
+            # if OP's comment is not "solved", flair submission as "contested"
+            elif not rh.already_contested(comment.submission) and not rh.already_solved(comment.submission):
+                db.save_post(comment.submission.id, 'contested')
+                rh.apply_flair(submission=comment.submission, text=config["flairs"]["contested"]["text"],
+                               flair_id=config["flairs"]["contested"]["id"])
+
+        # otherwise, if new non-OP comment on an "unknown", "contested" or "unsolved" submission,
+        # flair submission as "contested"
+        else:
+            submission_entry_in_db = db.check_post(
+                comment.submission.id)
+            if (
+                    submission_entry_in_db in ['unknown', 'contested', 'unsolved'] and
+                    comment.submission.link_flair_template_id not in (
+                    config["flairs"]["solved"]["id"],
+                    config["flairs"]["unsolved"]["id"],
+                    config["flairs"]["contested"]["id"])
+            ):
+                db.save_post(comment.submission.id, 'contested')
+                rh.apply_flair(
+                    comment.submission,
+                    config["flairs"]["contested"]["text"],
+                    config["flairs"]["contested"]["id"],
+                )
