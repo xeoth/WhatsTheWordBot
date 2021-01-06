7f9672f851f5faf3973c368d3241921b6841f197
Xeoth <46075322+Xeoth@users.noreply.github.com>
2021-01-06 18:23:38 +0100

    Moved checking contested posts to a separate file

--- src/routines/check_contested.py
+++ src/routines/check_contested.py
@@ -14,8 +14,38 @@
 #
 #  ---
 #
-#  Last modified by Xeoth on 2.1.2021
+#  Last modified by Xeoth on 06.01.2021
 #                   ^--------^ please change when modifying to comply with the license
 
-def check_contested():
-    pass
+import logging
+import praw
+from praw import exceptions
+import helpers
+
+
+def check_contested(reddit: praw.Reddit, db: helpers.DatabaseHelper, rh: helpers.RedditHelper, config):
+    old_contested_submissions = db.get_old_posts(status='contested', second_limit=config["contested_to_unknown"])
+
+    for submission_id in old_contested_submissions:
+        try:
+            # get submission object from id
+            submission = reddit.submission(submission_id)
+            
+            # check comments one last time for potential solve
+            if rh.mod_overridden(submission_id):
+                break
+            elif rh.solved_in_comments(submission=submission) or \
+                    rh.check_flair(submission=submission,
+                                   flair_text=config["flairs"]["solved"]["text"],
+                                   flair_id=config["flairs"]["solved"]["id"]):
+                db.save_post(submission_id, 'solved')
+                rh.apply_flair(
+                    submission=submission, text=config["flairs"]["solved"]["text"],
+                    flair_id=config["flairs"]["solved"]["id"])
+            else:
+                db.save_post(submission_id, 'unknown')
+                rh.apply_flair(
+                    submission=submission, text=config["flairs"]["solved"]["text"],
+                    flair_id=config["flairs"]["solved"]["id"])
+        except exceptions.PRAWException as e:
+            logging.error(f"Couldn't check old submission {submission_id}. {e}")
