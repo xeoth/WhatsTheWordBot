6774182874a0f7f45666f09e091d8658f5f3fac3
Xeoth <46075322+Xeoth@users.noreply.github.com>
2021-01-15 01:27:33 +0100

    Everything has been moved

--- src/routines/check_unsolved.py
+++ src/routines/check_unsolved.py
@@ -14,8 +14,42 @@
 #
 #  ---
 #
-#  Last modified by Xeoth on 2.1.2021
+#  Last modified by Xeoth on 15.1.2021
 #                   ^--------^ please change when modifying to comply with the license
 
-def check_unsolved():
-    pass
+import praw
+from praw import models
+import helpers
+import logging
+
+
+def check_unsolved(reddit: praw.Reddit, db: helpers.DatabaseHelper, rh: helpers.RedditHelper, config):
+    old_unsolved_submissions = db.get_old_posts(status='unsolved',
+                                                second_limit=config["unsolved_to_abandoned"])
+    
+    for entry in old_unsolved_submissions:
+        try:
+            # get submission object from id
+            submission = reddit.submission(id=entry)
+            # check comments one last time for potential solve
+            
+            if rh.mod_overridden(submission):
+                continue
+            
+            # only update flair if successfully updated in database, to avoid out-of-sync issues
+            if rh.solved_in_comments(submission=submission) or rh.check_flair(submission=submission,
+                                                                              flair_text=config["flairs"]["solved"][
+                                                                                  "text"],
+                                                                              flair_id=config["flairs"]["solved"][
+                                                                                  "id"]):
+                if db.save_post(post_id=entry, status='solved'):
+                    rh.apply_flair(
+                        submission=submission, text=config["flairs"]["solved"]["text"],
+                        flair_id=config["flairs"]["solved"]["id"])
+            else:
+                if db.save_post(post_id=entry, status='abandoned'):
+                    rh.apply_flair(
+                        submission=submission, text=config["flairs"]["abandoned"]["text"],
+                        flair_id=config["flairs"]["abandoned"]["id"])
+        except Exception as e:
+            logging.error(f"Couldn't check old submission {entry}. {e}")
